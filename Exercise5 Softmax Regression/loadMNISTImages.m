e0789f9ff94f714698d2389356a5dc9556b7f9e8
lucyfighting <bjfuln@163.com>
2015-03-23 15:08:32 +0800

    深度学习练习5
    softmax分类器

---
+++ Exercise5 Softmax Regression/loadMNISTImages.m
@@ -0,0 +1,31 @@
+function images = loadMNISTImages(filename)
+%loadMNISTImages returns a 28x28x[number of MNIST images] matrix containing
+%the raw MNIST images
+
+fp = fopen(filename, 'rb');
+assert(fp ~= -1, ['Could not open ', filename, '']);
+
+magic = fread(fp, 1, 'int32', 0, 'ieee-be');
+assert(magic == 2051, ['Bad magic number in ', filename, '']);
+
+numImages = fread(fp, 1, 'int32', 0, 'ieee-be');
+numRows = fread(fp, 1, 'int32', 0, 'ieee-be');
+numCols = fread(fp, 1, 'int32', 0, 'ieee-be');
+
+images = fread(fp, inf, 'unsigned char');
+images = reshape(images, numCols, numRows, numImages);
+%images = permute(images,[2 1 3])
+[a,b,c]=size(images);
+temp=zeros(a,b,c);
+for i=1:c
+temp(:,:,i)=images(:,:,i)';
+end
+images=temp;
+fclose(fp);
+
+% Reshape to #pixels x #examples
+images = reshape(images, size(images, 1) * size(images, 2), size(images, 3));
+% Convert to double and rescale to [0,1]
+images = double(images) / 255;
+
+end
